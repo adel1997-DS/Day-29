422b1c2908e81a7fa736e3f618105f53878b49f5
adel1997-DS <152065722+adel1997-DS@users.noreply.github.com>
2024-11-17 01:07:57 +0300

    Update main.py

--- main.py
+++ main.py
@@ -2,7 +2,6 @@ from fastapi import FastAPI
 from pydantic import BaseModel
 import joblib
 
-# تحميل النموذج والمقياس
 model = joblib.load(r"C:\Users\adeln\Desktop\A\Day-29\knn_model.joblib")
 scaler = joblib.load(r"C:\Users\adeln\Desktop\A\Day-29\scaler.joblib")
 
@@ -19,7 +18,6 @@ class InputFeatures(BaseModel):
     current_value: int
 
 def preprocessing(input_features: InputFeatures):
-    # إنشاء قاموس للميزات
     dict_f = {
         'appearance': input_features.appearance,
         'minutes_played': input_features.minutes_played,
@@ -27,20 +25,16 @@ def preprocessing(input_features: InputFeatures):
         'current_value': input_features.current_value
     }
     
-    # تحويل القيم إلى قائمة بالترتيب الصحيح
     features_list = [dict_f[key] for key in sorted(dict_f)]
     
-    # قياس الميزات باستخدام المقياس المحمل
     scaled_features = scaler.transform([features_list])
     
     return scaled_features
 
 @app.post("/predict")
 def predict(input_features: InputFeatures):
-    # معالجة البيانات
     processed_data = preprocessing(input_features)
     
-    # إجراء التنبؤ
     prediction = model.predict(processed_data)
     
     return {"prediction": prediction.tolist()}
